d715cf6caddc23f289b1fee77a7c0c3f3990311c
Allan Garcia <allan@apli.jobs>
2020-12-07 01:54:21 -0600

    Processed input document and get most similar

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,35 +1,29 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
+from difflib import SequenceMatcher
 from typing import Any, Dict, List, Text
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+
+
+DOCS = [
+    "reglamento interno",
+    "reglamento general de estudios",
+    "reglamento de titulacion profesional",
+    "ley organica",
+]
+
+
+def _str_sim(a, b):
+    return SequenceMatcher(None, a=a, b=b).ratio()
+
+
+def identify_document(doc):
+    sims = [_str_sim(d) for d in DOCS]
+    val, idx = min((val, idx) for (idx, val) in enumerate(sims))
+    if val < 6.5:
+        return -1
+
+    return DOCS[idx]
 
 
 class ActionExtractArticle(Action):
