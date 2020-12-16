84da994e3ab73565d7839f3af335f608d230423b
Allan Garcia <allan@apli.jobs>
2020-12-15 23:50:54 -0600

    Preparation script added

---
+++ lib/transform.py
@@ -0,0 +1,31 @@
+"""Funciones de transformacion de archivos."""
+
+from lib.legal_structures.base import identify_item
+from lib.legal_structures.legal_file import LegalFileStructure
+
+
+def estructurar_documento(fname: str, dir_out: str):
+    """Recibe un documento normatvo en formato TXT, lo transforma a JSON y lo escribe en el directorio
+    de salida `dir`."""
+    if not fname.endswith(".txt"):
+        raise ValueError(f"archivo {fname} tiene extension de archivo invalido.")
+
+    lfs = LegalFileStructure()
+    paragraphs = []
+    with open(fname) as input_file:
+        for line in input_file:
+            # Remove trailing whitespace.
+            line = line.strip()
+
+            # Parse all paragraphs as a single item
+            if line != "":
+                paragraphs.append(line)
+            else:
+                text = "\n".join(paragraphs)
+                paragraphs.clear()
+                item = identify_item(text)
+                if item:
+                    lfs.add_item(item)
+
+    fname_without_txt = fname[: fname.rfind(".txt")]
+    return lfs.write_file(fname_without_txt, dir_out)
---
+++ preparcion.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+Script de preparacion de documentos normativos
+
+Transforma los documentos en formato TXT a un formato estructurado JSON. Este script itera
+sobre un directorio y 
+"""
+
+import argparse
+import os
+
+from lib.transform import estructurar_documento
+
+parser = argparse.ArgumentParser(
+    description="Script de preparacion de documentos normativos de formato TXT a JSON."
+)
+parser.add_argument(
+    "--dir_txt",
+    help="Directorio que contiene los archivos de entrada en formtato TXT.",
+    default="docs/txt/",
+    required=False,
+)
+parser.add_argument(
+    "--dir_sal",
+    help="Directorio de salida en donde se escribirán los archivos resultantes en formato JSON.",
+    default="docs/json/",
+    required=False,
+)
+args = parser.parse_args()
+
+# Verificacion de directorios de entrada y salida
+if not os.path.exists(args.dir_txt):
+    print(f"El directorio {args.dir_txt} no existe.")
+    exit(1)
+elif not os.path.isdir(args.dir_txt):
+    print(f"La ruta especificada {args.dir_txt} no es un directorio válido.")
+    exit(1)
+
+# Si no existe el directorio de salida, se crea
+if not os.path.exists(args.dir_sal):
+    try:
+        os.makedirs(args.dir_sal)
+    except PermissionError:
+        print(f"No se pudo crear el archivo destino {args.dir_sal}; error de permisos.")
+        exit(1)
+
+print(f"Leyendo archivos del directorio {args.dir_txt}...\n")
+for d in os.listdir(args.dir_txt):
+    print(f"- {d}")
+    fname = f"{os.path.realpath(args.dir_txt)}/{d}"
+    try:
+        estructurar_documento(fname, args.dir_sal)
+    except ValueError as ve:
+        print(f"\tOmitiendo archivo: {ve}")
